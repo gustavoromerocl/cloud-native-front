c3d670b3141f12b96cc2e79e23bf98131d3751d2
gustavoromerocl <gandresrp@gmail.com>
2025-01-11 16:45:21 -0300

    feat: add msal config integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,43 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideStore } from '@ngrx/store';
+import { sessionReducer } from './store/session/session.reducer';
+import { MSAL_GUARD_CONFIG, MSAL_INSTANCE, MsalBroadcastService, MsalGuard, MsalInterceptor, MsalService } from '@azure/msal-angular';
+import { InteractionType, PublicClientApplication } from '@azure/msal-browser';
+
+
+const MSALInstanceFactory = () =>
+  new PublicClientApplication({
+    auth: {
+      clientId: '858b4bc0-7115-4192-a217-5de9275c3ab1',
+      authority: 'https://login.microsoftonline.com/6594e534-4a19-4085-a8e1-411ef99af367',
+      redirectUri: window.location.origin,
+    },
+  });
+
+const MSALGuardConfigFactory = () => ({
+  interactionType: InteractionType.Redirect, // Usa el flujo de redirección
+  authRequest: {
+    scopes: ['user.read'], // Solicitar permisos necesarios
+  },
+});
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimations(), provideAnimationsAsync(),]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideAnimations(), 
+    provideAnimationsAsync(),
+    provideStore({
+      session: sessionReducer,
+    }),
+    { provide: MSAL_INSTANCE, useFactory: MSALInstanceFactory }, // Proveedor de MSAL Instance
+    { provide: MSAL_GUARD_CONFIG, useFactory: MSALGuardConfigFactory }, // Configuración de MSAL Guard
+    MsalService, // Servicio de MSAL
+    MsalGuard, // Guard de MSAL
+    MsalBroadcastService, // Servicio de transmisión de MSAL
+  ]
 };
