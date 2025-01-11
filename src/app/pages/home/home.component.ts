841db186e52782e8ad3ec91d99ecb5aa2301e521
gustavoromerocl <gandresrp@gmail.com>
2025-01-11 18:57:10 -0300

    feat: set api test on home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  posts: any[] = [];
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {}
+
+  getPosts(): void {
+    this.apiService.getPosts().subscribe((data) => {
+      console.log('Posts:', data);
+      this.posts = data;
+    });
+  }
+
+  createPost(): void {
+    const newPost = { title: 'Nuevo Post', body: 'Contenido del post', userId: 1 };
+    this.apiService.createPost(newPost).subscribe((data) => {
+      console.log('Post creado:', data);
+      this.posts.push(data);
+    });
+  }
+
+  updatePost(): void {
+    const updatedPost = { title: 'Post Actualizado', body: 'Contenido actualizado' };
+    this.apiService.updatePost(1, updatedPost).subscribe((data) => {
+      console.log('Post actualizado:', data);
+    });
+  }
+
+  deletePost(): void {
+    this.apiService.deletePost(1).subscribe(() => {
+      console.log('Post eliminado');
+      this.posts = this.posts.filter((post) => post.id !== 1);
+    });
+  }
 }
