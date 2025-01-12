095bdc1363ca0d9df84957f0e9677818d3143a87
gustavoromerocl <gandresrp@gmail.com>
2025-01-12 14:48:19 -0300

    refactor: set close button styles on modal

--- src/app/components/vital-signs-dialog/vital-signs-dialog.component.ts
+++ src/app/components/vital-signs-dialog/vital-signs-dialog.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 @Component({
@@ -9,7 +10,7 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
   templateUrl: './vital-signs-dialog.component.html',
   styleUrls: ['./vital-signs-dialog.component.scss'],
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatDialogModule, MatButtonModule],
+  imports: [CommonModule, MatTableModule, MatDialogModule, MatButtonModule, MatIconModule],
 })
 export class VitalSignsDialogComponent implements OnInit {
   displayedColumns = ['type', 'value', 'unit', 'measurementDate'];
