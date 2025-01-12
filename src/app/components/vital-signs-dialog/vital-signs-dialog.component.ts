f2bf825a589ca94cf5b5dab6d57f29dac822b383
gustavoromerocl <gandresrp@gmail.com>
2025-01-12 14:33:48 -0300

    feat: create vital signs modal component

---
+++ src/app/components/vital-signs-dialog/vital-signs-dialog.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+
+@Component({
+  selector: 'app-vital-signs-dialog',
+  templateUrl: './vital-signs-dialog.component.html',
+  styleUrls: ['./vital-signs-dialog.component.scss'],
+  standalone: true,
+  imports: [CommonModule, MatTableModule, MatDialogModule, MatButtonModule],
+})
+export class VitalSignsDialogComponent implements OnInit {
+  displayedColumns = ['type', 'value', 'unit', 'measurementDate'];
+  dataSource = new MatTableDataSource<any>();
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any[]) {
+    this.dataSource.data = this.data;
+  }
+
+  ngOnInit() {
+    this.dataSource.data = this.data;
+  }
+}
