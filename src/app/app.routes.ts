8cbfdb90c8dd4ce2fa74ae6616181f14900cd28d
gustavoromerocl <gandresrp@gmail.com>
2025-01-11 21:23:11 -0300

    feat: implement resolve on lauot component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,21 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { MsalGuard } from '@azure/msal-angular';
+import { SessionResolver } from './resolvers/session.resolver';
 
 export const routes: Routes = [
   {
     path: '',
     component: LayoutComponent, // El layout es el componente principal
+    resolve: {
+      session: SessionResolver // Resuelve el session storage antes de cargar el layout
+    },
     canActivate: [MsalGuard],
     children: [
       {
         path: '',
         loadComponent: () =>
-          import('./pages/dashboard/home.component').then((m) => m.HomeComponent),
+          import('./pages/home/home.component').then((m) => m.HomeComponent),
         pathMatch: 'full',
       },
       {
